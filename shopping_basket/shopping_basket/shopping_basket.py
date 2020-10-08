7878b4e553b1269d587156b53d83a5ac89911078
Anthony <leunga1000@yahoo.com>
2020-10-08 11:09:22 +0100

    Fixed initialisation for Basket in tests

--- shopping_basket/shopping_basket/shopping_basket.py
+++ shopping_basket/shopping_basket/shopping_basket.py
@@ -9,6 +9,10 @@ class ItemNotFoundInCatalogueException(Exception):
     pass
 
 
+class NegativePriceException(Exception):
+    pass
+
+
 class Basket:
     def __init__(self, items):
         self.items = Counter(items)
@@ -18,26 +22,29 @@ class Basket:
 
 
 class Offer:
-    def compute_discount(self, basket: Dict[str, int], catalogue: Dict[str, float]):
+    def compute_discount(self, basket: Basket, catalogue: Dict[str, float]):
         pass
 
 
 class BasketPricer:
-    def __init__(self, basket: Dict[str, int], catalogue: Dict[str, float], offers: List[Offer]):
+    def __init__(self, basket: Basket, catalogue: Dict[str, float], offers: List[Offer]):
         self.basket = basket
         self.catalogue = catalogue
         self.offers = offers
-        for item in self.basket.items():
+        for item in self.basket.items:
             if item not in self.catalogue:
+                print(f"{item}")
                 raise ItemNotFoundInCatalogueException(item)
 
     def totals(self):
         subtotal, discount = self.subtotal(), self.discount()
         total = subtotal - discount
+        if total < 0.0:
+            raise NegativePriceException
         return subtotal, discount, total
 
     def subtotal(self):
-        return sum(qty * self.catalogue[item] for item, qty in self.basket.items())
+        return sum(qty * self.catalogue[item] for item, qty in self.basket.items.items())
 
     def discount(self):
         return sum(offer.compute_discount(self.basket, self.catalogue) for offer in self.offers)
--- shopping_basket/shopping_basket_tests/test_shopping_basket.py
+++ shopping_basket/shopping_basket_tests/test_shopping_basket.py
@@ -11,8 +11,18 @@ def test_add_basket():
 
 
 def test_empty_basket():
-    basket_pricer = BasketPricer(basket={}, catalogue={}, offers=[])
+    basket_pricer = BasketPricer(basket=Basket({}), catalogue={}, offers=[])
     subtotal, discount, total = basket_pricer.totals()
     assert subtotal == 0.0
     assert discount == 0.0
     assert total == 0.0
+
+
+def test_single_item():
+    basket = Basket({'baked_beans': 1})
+    catalogue = {'baked_beans': 0.99}
+    basket_pricer = BasketPricer(basket=basket, catalogue=catalogue, offers=[])
+    subtotal, discount, total = basket_pricer.totals()
+    assert subtotal == 0.99
+    assert discount == 0.0
+    assert total == 0.99
